05bb26c887ed3e01eb9534701181dc6c750cf47f
karnponpoochitkanon <karnpon.p@ku.th>
2025-11-26 18:47:10 +0700

    fix: add username

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=20)  
     fname = serializers.CharField()
     lname = serializers.CharField()
     bod = serializers.CharField() 
@@ -11,15 +12,15 @@ class SignupSerializer(serializers.Serializer):
     class Meta:
         swagger_schema_fields = {
             "example": {
+                "username": "pattrick",
                 "fname": "Pattrick",
                 "lname": "Loveson",
-                "bod": "02/01/2004",  
+                "bod": "02/01/2004",
                 "phone": "0863380481",
-                "email": "paranyu@gmail.com",
+                "email": "parunyu@gmail.com",
                 "password": "12345"
             }
         }
-
 
 class LoginSerializer(serializers.Serializer):
     identifier = serializers.CharField()
